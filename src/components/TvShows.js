ae208f90ac86a2c9eb3803c3bc1cac321f40a691
Tiffany Wong <tif.wong@yahoo.com>
2019-12-04 17:37:00 -0500

    trying out

--- src/components/TvShows.js
+++ src/components/TvShows.js
@@ -28,34 +28,34 @@ class TvShows extends Component {
     render() {
         return (
             <div>
-            <h1> Not So Social </h1>
+                <h1> Not So Social </h1>
 
-            <div className="dropdownDays">
-                <select name="days" id="days">
-                    <option value="Monday">Monday</option>
-                    <option value="Tuesday">Tuesday</option>
-                    <option value="Wednesday">Wednesday</option>
-                    <option value="Thursday">Thursday</option>
-                    <option value="Friday">Friday</option>
-                    <option value="Saturday">Saturday</option>
-                    <option value="Sunday">Sunday</option>
-                </select>
+                <div className="dropdownDays">
+                    <select name="days" id="days">
+                        <option value="Monday">Monday</option>
+                        <option value="Tuesday">Tuesday</option>
+                        <option value="Wednesday">Wednesday</option>
+                        <option value="Thursday">Thursday</option>
+                        <option value="Friday">Friday</option>
+                        <option value="Saturday">Saturday</option>
+                        <option value="Sunday">Sunday</option>
+                    </select>
 
-                <select name="genres" id="genres">
-                    <option value="Action">Action</option>
-                    <option value="Adventure">Adventure</option>
-                    <option value="Anime">Anime</option>
-                    <option value="Comedy">Comedy</option>
-                    <option value="Drama">Drama</option>
-                    <option value="Crime">Crime</option>
-                    <option value="Family">Family</option>
-                    <option value="Myster">Mystery</option>
-                    <option value="Romance">Romance</option>
-                    <option value="Science-Fiction">Science-Fiction</option>
-                    <option value="Thriller">Thriller</option>
-                </select>
+                    <select name="genres" id="genres">
+                        <option value="Action">Action</option>
+                        <option value="Adventure">Adventure</option>
+                        <option value="Anime">Anime</option>
+                        <option value="Comedy">Comedy</option>
+                        <option value="Drama">Drama</option>
+                        <option value="Crime">Crime</option>
+                        <option value="Family">Family</option>
+                        <option value="Myster">Mystery</option>
+                        <option value="Romance">Romance</option>
+                        <option value="Science-Fiction">Science-Fiction</option>
+                        <option value="Thriller">Thriller</option>
+                    </select>
 
-            </div>
+                </div>
             </div>
         )
     }
