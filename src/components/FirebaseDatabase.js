7c772ce563cbb306c3f1b66ad2f7845b48f208d3
Nick Wang <nickyiliwang@gmail.com>
2019-12-04 16:58:34 -0500

    diplaying items

--- src/components/FirebaseDatabase.js
+++ src/components/FirebaseDatabase.js
@@ -1,18 +1,53 @@
 import React, { Component } from "react";
 import Firebase from "../util/config";
-import 'firebase/database'
+import "firebase/database";
 
-const db = Firebase.database().ref('events');
+const db = Firebase.database().ref("events");
 
 export default class FirebaseDatabase extends Component {
+  constructor() {
+    super();
+    this.state = {
+      allEventsArray: [],
+      loading: false
+    };
+  }
+
   componentDidMount() {
     db.once("value", snapshot => {
       const data = snapshot.val();
-      console.log(data);
+      this.setState({ loading: true });
+      for (const key in data) {
+        if (data.hasOwnProperty(key)) {
+          const event = data[key];
+          const eventObj = {
+            name: key,
+            type: event.type,
+            partySize: event.partySize
+          };
+          this.setState({
+            allEventsArray: [...this.state.allEventsArray, eventObj]
+          });
+        }
+      }
+      this.setState({ loading: false });
     });
   }
 
+  renderEvents = () => {
+    return this.state.allEventsArray.map(event => {
+      console.log(event);
+      return (
+        <li key={event.name}>
+          <h2>{event.name}</h2>
+          <p>type: {event.type}</p>
+          <p>party size: {event.partySize}</p>
+        </li>
+      );
+    });
+  };
+
   render() {
-    return <div></div>;
+    return <ul>{!this.state.loading && this.renderEvents()}</ul>;
   }
 }
