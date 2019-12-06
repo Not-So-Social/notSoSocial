b4d696bd0a225651d85eeec76065ee62177fcda8
Lewis <lewisbrignell@gmail.com>
2019-12-06 13:04:07 -0500

    added styles to TV show list via new  tvShows.scss

--- src/components/TvShows.js
+++ src/components/TvShows.js
@@ -140,7 +140,7 @@ class TvShows extends Component {
 
                 // rendering the li
                 return (
-                    <li key={id}>
+                    <li className="tvShowListItem" key={id}>
                         <button
                             onClick={() => this.props.retrieveTvShowClicked(newTvShowObjectToDisplay)}
                         >
