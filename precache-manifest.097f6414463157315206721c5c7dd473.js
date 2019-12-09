e502edc62ca57f5dfe2f6fb06c212a8515bfa445
Nick Wang <nickyiliwang@gmail.com>
2019-12-09 11:16:16 -0500

    Updates

---
+++ precache-manifest.097f6414463157315206721c5c7dd473.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa61bb059735494e5e8795fcb2b55b65",
+    "url": "/notSoSocial/index.html"
+  },
+  {
+    "revision": "fecd47805afb66fa12bd",
+    "url": "/notSoSocial/static/css/main.4956d42b.chunk.css"
+  },
+  {
+    "revision": "c90336adb0f988d45327",
+    "url": "/notSoSocial/static/js/2.a8cdcc01.chunk.js"
+  },
+  {
+    "revision": "fecd47805afb66fa12bd",
+    "url": "/notSoSocial/static/js/main.a2df2232.chunk.js"
+  },
+  {
+    "revision": "edc9b8950c9d629fec9c",
+    "url": "/notSoSocial/static/js/runtime-main.f771d3c2.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/notSoSocial/precache-manifest.cb64c919b3c6ec05113bbc71b022a204.js"
+  "/notSoSocial/precache-manifest.097f6414463157315206721c5c7dd473.js"
 );
 
 self.addEventListener('message', (event) => {
