9c61423385c6fa6d6603750c6b83eef7d8e93591
Nick Wang <nickyiliwang@gmail.com>
2019-12-09 14:14:38 -0500

    scroll to top

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,11 @@
 import React from "react";
+import ScrollUpButton from "react-scroll-up-button";
 
 export default function Footer() {
   return (
     <footer>
-      <p>Created by: peopel</p>
+      <p>Created by: people</p>
+      <ScrollUpButton />
     </footer>
   );
 }
