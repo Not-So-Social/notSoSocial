af8f3a5724daf1768ddb2bbe2c1a367f75034279
Tiffany <tif.wong@yahoo.com>
2019-12-10 10:30:47 -0500

    add footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ import ScrollUpButton from "react-scroll-up-button";
 export default function Footer() {
   return (
     <footer>
-      <p>Created by: people</p>
+      <p>Created by: <a href="https://github.com/Not-So-Social/notSoSocial"> Not So Social People</a> </p>
       <ScrollUpButton />
     </footer>
   );
