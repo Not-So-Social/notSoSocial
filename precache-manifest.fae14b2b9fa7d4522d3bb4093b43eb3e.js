abe1a78a9e0594ca6474e61de322d8da3d154c3d
hira <hiraahsan94@gmail.com>
2019-12-10 17:07:40 -0500

    Updates

---
+++ precache-manifest.fae14b2b9fa7d4522d3bb4093b43eb3e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "779f52243e9a335972d404c9deb50df5",
+    "url": "/notSoSocial/index.html"
+  },
+  {
+    "revision": "f74be54c78a917a9c9b2",
+    "url": "/notSoSocial/static/css/main.cbd62847.chunk.css"
+  },
+  {
+    "revision": "598db413c7eacc6ebd84",
+    "url": "/notSoSocial/static/js/2.25249325.chunk.js"
+  },
+  {
+    "revision": "f74be54c78a917a9c9b2",
+    "url": "/notSoSocial/static/js/main.0eab6a4e.chunk.js"
+  },
+  {
+    "revision": "edc9b8950c9d629fec9c",
+    "url": "/notSoSocial/static/js/runtime-main.f771d3c2.js"
+  },
+  {
+    "revision": "0a2ccae99087fb89045900584ec5adb7",
+    "url": "/notSoSocial/static/media/tvShowCollage.0a2ccae9.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/notSoSocial/precache-manifest.9b4df6efbe659bd6f9a697df98aac2a4.js"
+  "/notSoSocial/precache-manifest.fae14b2b9fa7d4522d3bb4093b43eb3e.js"
 );
 
 self.addEventListener('message', (event) => {
