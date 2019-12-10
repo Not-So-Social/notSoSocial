d09d6c4753e7bd6c5b7773a50cbac83f3e782ab6
Tiffany <tif.wong@yahoo.com>
2019-12-10 11:03:17 -0500

    change footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ import ScrollUpButton from "react-scroll-up-button";
 export default function Footer() {
   return (
     <footer>
-      <p>Created by: <a href="https://github.com/Not-So-Social/notSoSocial"> Not So Social People</a> </p>
+      <p>Created by: <a href="https://github.com/Not-So-Social/notSoSocial"> Hira Ahsan, Lewis Brignell, Nick Wang, Tiffany Wong</a> </p>
       <ScrollUpButton />
     </footer>
   );
